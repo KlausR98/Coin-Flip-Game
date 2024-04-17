a550df51e4f49ef46847f431ff35087366befbb7
KlausR98 <szymoon_rodak@o2.pl>
2024-04-17 13:19:19 +0200

    changes after review

--- script.js
+++ script.js
@@ -4,37 +4,15 @@ const resetButton = document.querySelector("#reset-button");
 let heads = 0;
 let tails = 0;
 
-// flipButton.addEventListener("click", () => flipCoin(coin, heads, tails));
 resetButton.addEventListener("click", () => {
   heads = 0;
   tails = 0;
   changeScore();
 });
 
-// function flipCoin(coin, heads, tails) {
-//   let i = Math.floor(Math.random() * 20);
-//   coin.style.animation = "none";
-//   if (i) {
-//     setTimeout(function () {
-//       coin.style.animation = "head-spin 3s forwards";
-//     }, 100);
-//     heads++;
-//   } else {
-//     setTimeout(function () {
-//       coin.style.animation = "tail-spin 3s forwards";
-//     }, 100);
-//     tails++;
-//   }
-//   setTimeout(changeScore, 3000);
-//   turnOffFlipButton();
-// }
-
 flipButton.addEventListener("click", () => {
   let i = Math.floor(Math.random() * 2);
   coin.style.animation = "none";
-  // if (heads++) {
-
-  // }
   if (i) {
     setTimeout(function () {
       coin.style.animation = "head-spin 3s forwards";
