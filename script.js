715408b4a9e8ef37b9179222220f572b30afa080
KlausR98 <szymoon_rodak@o2.pl>
2024-04-17 12:59:03 +0200

    javascript script added , style.css updated

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log("commit 1");
+const coin = document.querySelector(".coin");
+const flipButton = document.querySelector("#flip-button");
+const resetButton = document.querySelector("#reset-button");
+let heads = 0;
+let tails = 0;
+
+// flipButton.addEventListener("click", () => flipCoin(coin, heads, tails));
+resetButton.addEventListener("click", () => {
+  heads = 0;
+  tails = 0;
+  changeScore();
+});
+
+// function flipCoin(coin, heads, tails) {
+//   let i = Math.floor(Math.random() * 20);
+//   coin.style.animation = "none";
+//   if (i) {
+//     setTimeout(function () {
+//       coin.style.animation = "head-spin 3s forwards";
+//     }, 100);
+//     heads++;
+//   } else {
+//     setTimeout(function () {
+//       coin.style.animation = "tail-spin 3s forwards";
+//     }, 100);
+//     tails++;
+//   }
+//   setTimeout(changeScore, 3000);
+//   turnOffFlipButton();
+// }
+
+flipButton.addEventListener("click", () => {
+  let i = Math.floor(Math.random() * 2);
+  coin.style.animation = "none";
+  // if (heads++) {
+
+  // }
+  if (i) {
+    setTimeout(function () {
+      coin.style.animation = "head-spin 3s forwards";
+    }, 100);
+    heads++;
+  } else {
+    setTimeout(function () {
+      coin.style.animation = "tail-spin 3s forwards";
+    }, 100);
+    tails++;
+  }
+  setTimeout(changeScore, 3000);
+  turnOffFlipButton();
+});
+
+function turnOffFlipButton() {
+  flipButton.disabled = true;
+  setTimeout(function () {
+    flipButton.disabled = false;
+  }, 3000);
+}
+
+function changeScore() {
+  document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+  document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+}
